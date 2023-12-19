f59655d37edd5901444c6fef14c5cdd2c5e76980
shubhamy <shubhamy@ohumhealthcare.com>
2023-12-19 12:17:40 +0530

    Install required dependencies

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app Welcome!</Text>
+      <Text style={styles.whiteText}>"Vatavaran" brings you real-time weather updates in a visually stunning React Native interface. From the golden glow of sunshine to the gentle patter of raindrops, we've crafted an immersive experience to keep you connected to the elements. 📲☔</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  whiteText:{
+    color:'#f0f0f0'
+  },
+  text:{
+    color:"#fff",
+    fontSize:34,
+    fontWeight:600
+  }
 });
