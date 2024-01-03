d8605dbbb55a3e85f06f36f7645819495f58aad8
shubhamy <shubhamy@ohumhealthcare.com>
2024-01-03 15:04:17 +0530

    Add Loader component using Animated

--- components/Loader.js
+++ components/Loader.js
@@ -1,37 +1,58 @@
-import {View, Text, StyleSheet, Animated} from 'react-native'
-import React from 'react'
+import React, { useRef, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { Animated, Text, View, Easing } from 'react-native';
 
+const styles = StyleSheet.create({
+    outer: {
+        width: 25,
+        height: 25,
+        borderRadius: 100,
+        borderWidth: 5,
+        borderColor: "#4a8be0",
+        borderBottomColor: "#fff",
+        borderTopColor: "#fff",
+        // position: 'absolute',
+    }
+});
+// You can then use your `FadeInView` in place of a `View` in your components:
 const Loader = () => {
-    const styles = StyleSheet.create({
-        outer: {
-            width: 25,
-            height: 25,
-            borderRadius: 100,
-            borderWidth: 5,
-            borderColor: "#4a8be0",
-            borderBottomColor: "#fff",
-            borderTopColor: "#fff",
-            position: 'absolute',
-            top: '50%',      // Adjust vertical positioning as needed
-            left: '50%',     // Adjust horizontal positioning as needed
-            marginLeft: -12.5,  // Adjust for half the width
-            marginTop: -12.5,   // Adjust for half the height
-            // transform: [{
-            //     rotate: Animated.loop(
-            //         new Animated.Value(0).interpolate({
-            //             inputRange: [0, 1],
-            //             outputRange: ['0deg', '360deg']
-            //         })
-            //     )._value // Convert Animated.Value to string
-            // }]
-        }
+    let rotateValueHolder = new Animated.Value(0);
+
+    const startImageRotateFunction = () => {
+        rotateValueHolder.setValue(0);
+        Animated.timing(rotateValueHolder, {
+            toValue: 1,
+            duration: 500,
+            easing: Easing.linear,
+            useNativeDriver: false,
+        }).start(() => startImageRotateFunction());
+    };
+
+    const rotateData = rotateValueHolder.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '360deg'],
     });
 
+    useEffect(() => {
+        startImageRotateFunction();
+    }, [])
+    
+
     return (
-        <View style={styles.outer}>
-            
+        <View
+            style={{
+                // flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+            <Animated.View // Special animatable View
+                style={{
+                    ...styles.outer,
+                    transform: [{ rotate: rotateData }], // Bind opacity to animated value
+                }}>
+            </Animated.View>
         </View>
-    )
-}
+    );
+};
 
 export default Loader
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,41 +1,36 @@
-import {View, Text, Image, TouchableOpacity} from 'react-native';
-import React from 'react';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {themeColors} from '../theme';
-import {useNavigation} from '@react-navigation/native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { themeColors } from '../theme';
+import { useNavigation } from '@react-navigation/native';
 import Loader from '../components/Loader';
 
 export default function WelcomeScreen() {
+    const [showLoader, setshowLoader] = useState(true);
     const navigation = useNavigation();
     return (
-        <SafeAreaView className="flex-1" style={{backgroundColor: themeColors.black_1}}>
+        <SafeAreaView className="flex-1" style={{ backgroundColor: themeColors.black_1 }}>
             <View className="flex-1 flex justify-around my-4">
                 <View className="flex-col justify-center items-center">
                     <Image source={require("../assets/images/atmos2.png")}
-                        style={{width: 300, height: 100, resizeMode: 'contain'}} />
+                        style={{ width: 300, height: 100, resizeMode: 'contain' }} />
                     <Image source={require("../assets/images/login2.png")}
-                        style={{width: 300, height: 300, resizeMode: 'contain'}} />
+                        style={{ width: 300, height: 300, resizeMode: 'contain' }} />
                 </View>
-                {/* <View>
-                    <Text
-                        className="font-bold text-4xl text-center" style={{color: themeColors.white_1}}>
-                        Let's Get Started!
-                    </Text>
-                    <Text className="text-gray-300 text-center text-lg text-yellow-400" >
-                        <Text className="font-bold">"AtmosApp"</Text> brings you real-time weather updates on a few steps.
-                    </Text>
-                </View> */}
                 <View className="space-y-4">
                     <TouchableOpacity
                         onPress={() => navigation.navigate('Signup')}
-                        className="py-3 mx-7 rounded-xl" style={{backgroundColor:themeColors.blue_1,position:"relative"}}>
-                        <Text
-                            className="text-xl font-bold text-center"
-                            style={{color:themeColors.white_1}}
-                        >
-                            Sign Up
-                            <Loader></Loader>
-                        </Text>
+                        className="py-3 mx-7 rounded-xl" style={{ backgroundColor: themeColors.blue_1, position: "relative" }}>
+                        {
+                            showLoader
+                                ? <Loader></Loader>
+                                : <Text
+                                    className="text-xl font-bold text-center"
+                                    style={{ color: themeColors.white_1 }}
+                                >
+                                    Sign Up
+                                </Text>
+                        }
                     </TouchableOpacity>
                     <View className="flex-row justify-center">
                         <Text className="text-white font-semibold">Already have an account?</Text>
